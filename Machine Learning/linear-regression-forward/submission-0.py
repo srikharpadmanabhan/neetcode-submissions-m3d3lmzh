d496e3edf92bb28a71f209d5635532fef2dd1092
srikharpadmanabhan <65561220+srikharpadmanabhan@users.noreply.github.com>
2026-03-30 13:15:50 -0400

    Add: linear-regression-forward - submission-0

---
+++ Machine Learning/linear-regression-forward/submission-0.py
@@ -0,0 +1,14 @@
+import numpy as np
+from numpy.typing import NDArray
+
+class Solution:
+
+    def get_model_prediction(self, X: NDArray[np.float64], weights: NDArray[np.float64]) -> NDArray[np.float64]:
+        y_hat = X @ weights
+        print(y_hat.shape, y_hat)
+        return np.round(y_hat,5)
+
+    def get_error(self, model_prediction: NDArray[np.float64], ground_truth: NDArray[np.float64]) -> float:
+        mse = np.mean(np.square(model_prediction - ground_truth))
+
+        return np.round(mse,5)
